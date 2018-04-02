97ebc0319be2fed3be5fcb347df7b39edddbef08
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-02 17:19:35 +0100

    made second spec and setup

---
+++ specs/spec2.rb
@@ -0,0 +1,12 @@
+require('minitest/autorun')
+require_relative('../classes_pratice2')
+
+class TestClassesPraticeTwo < MiniTest::Test
+def setup()
+ @Team = Team.new("Celtic", ["Bill","Ben"], "Callum")
+end
+
+
+
+
+end
