3f6d5b92f13cbd10944c3786f5a1b0c85ad39b7b
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-02 16:08:35 +0100

    completed set student name

--- specs/spec1.rb
+++ specs/spec1.rb
@@ -6,13 +6,16 @@ class TestClassesPratice < MiniTest::Test
 def setup()
   @Student = Student.new("Callum", 5)
 end
-def test_student_name
+def test_student_name__Callum
   assert_equal("Callum", @Student.student_name)
   end
-def test_student_cohort
+def test_student_cohort__5
   assert_equal(5, @Student.student_cohort)
 end
-
+def test_set_student_name
+  @Student.student_name = "Alan"
+  assert_equal("Alan", @Student.student_name)
+end
 
 
 
