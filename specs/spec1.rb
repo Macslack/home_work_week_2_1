907793a162dce87d494afd5350697e2aba477481
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-02 16:01:10 +0100

    completed student name and cohort getter

--- specs/spec1.rb
+++ specs/spec1.rb
@@ -6,3 +6,17 @@ class TestClassesPratice < MiniTest::Test
 def setup()
   @Student = Student.new("Callum", 5)
 end
+def test_student_name
+  assert_equal("Callum", @Student.student_name)
+  end
+def test_student_cohort
+  assert_equal(5, @Student.student_cohort)
+end
+
+
+
+
+
+
+
+end
