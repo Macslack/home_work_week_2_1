0d81911ce0c324d94e0aacc2d8d314418e31643a
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-02 18:19:29 +0100

    completed up to set coach

--- classes_pratice2.rb
+++ classes_pratice2.rb
@@ -1,17 +1,26 @@
 class Team
+  attr_accessor :name, :players, :coach_name
 def initialize(name, players, coach_name)
   @name = name
   @players = players
   @coach_name = coach_name
 end
-def team_name
-  return @name
-end
-def player_name
-  return @players
-end
 
-
+# def team_name
+#   return @name
+# end
+# def player_name
+#   return @players
+# end
+# def coach_name
+#   return @coach_name
+# end
+# def set_coach_name(new_coach_name)
+#   @coach_name = new_coach_name
+# end
+def add_player()
+  @players == players.unshift("Caz")
+end
 
 
 
--- specs/spec2.rb
+++ specs/spec2.rb
@@ -6,10 +6,20 @@ class TestClassesPraticeTwo < MiniTest::Test
     @Team = Team.new("Celtic", ["Bill","Ben"], "Callum")
   end
   def test_team_name__Celtic
-    assert_equal("Celtic", @Team.team_name)
+    assert_equal("Celtic", @Team.name)
   end
   def test_player_name__Bill_Ben
-    assert_equal(["Bill","Ben"], @Team.player_name)
+    assert_equal(["Bill","Ben"], @Team.players)
   end
-
+  def test_coach_name
+    assert_equal("Callum", @Team.coach_name)
+  end
+  def test_set_coach_name
+    @Team.coach_name = "Alan"
+    assert_equal("Alan", @Team.coach_name)
+  end
+  def test_add_player
+    @Team.add_player("Caz")
+    assert_equal(["Caz","Bill","Ben"], @Team.players)
+  end
 end
