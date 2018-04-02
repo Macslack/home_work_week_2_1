0e755c8beb3e3a47067be173e7f18e3337386d84
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-02 15:45:31 +0100

    created Student class & initialize method

---
+++ classes_pratice.rb
@@ -0,0 +1,5 @@
+class Student
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+end
--- specs/spec1.rb
+++ specs/spec1.rb
@@ -2,3 +2,7 @@ require('minitest/autorun')
 require_relative('../classes_pratice.rb')
 
 class TestClassesPratice < MiniTest::Test
+
+def setup()
+  @Student = Student.new("Callum", 5)
+end
