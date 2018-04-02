a223c7842001b61d6b787386a922a0cac86bb56b
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-02 16:09:13 +0100

    completed set student name

--- classes_pratice.rb
+++ classes_pratice.rb
@@ -2,4 +2,21 @@ class Student
   def initialize(name, cohort)
     @name = name
     @cohort = cohort
+  end
+
+  def student_name()
+  return @name
+  end
+
+  def student_cohort()
+  return @cohort
+  end
+
+  def set_student_name(name)
+    @name = name
+  end
+
+
+
+
 end
