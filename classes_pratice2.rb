aac4811095d09cae61e4511615a0fd066f0e7d28
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-02 17:40:23 +0100

    made getter method for player array

---
+++ classes_pratice2.rb
@@ -0,0 +1,26 @@
+class Team
+def initialize(name, players, coach_name)
+  @name = name
+  @players = players
+  @coach_name = coach_name
+end
+def team_name
+  return @name
+end
+def player_name
+  return @players
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- specs/spec2.rb
+++ specs/spec2.rb
@@ -1,12 +1,15 @@
 require('minitest/autorun')
-require_relative('../classes_pratice2')
+require_relative('../classes_pratice2.rb')
 
 class TestClassesPraticeTwo < MiniTest::Test
-def setup()
- @Team = Team.new("Celtic", ["Bill","Ben"], "Callum")
-end
-
-
-
+  def setup()
+    @Team = Team.new("Celtic", ["Bill","Ben"], "Callum")
+  end
+  def test_team_name__Celtic
+    assert_equal("Celtic", @Team.team_name)
+  end
+  def test_player_name__Bill_Ben
+    assert_equal(["Bill","Ben"], @Team.player_name)
+  end
 
 end
